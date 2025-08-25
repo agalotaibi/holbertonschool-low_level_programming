9d4a8e633cde1b03a8eb312f7d59d6f5a4060813
agalotaibi <hopes.gmo@gmail.com>
2025-08-25 15:57:19 +0000

    solve task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,14 +21,14 @@ lastDig = n % 10;
 printf("Last digit of %d is %d ", n, lastDig);
 if (lastDig > 5)
 {
-printf("and is greater than 5"\n);
+printf("and is greater than 5\n");
 }
 else if (lastDig == 0)
 {
-printf("and is 0"\n);
+printf("and is 0\n");
 else
 {
-printf("and is less than 6 and not 0");
+printf("and is less than 6 and not 0\n");
 }
 return (0);
 }
