171b46d97aa3533f6f6c5be7ac45fd75da959ac9
agalotaibi <hopes.gmo@gmail.com>
2025-08-26 07:45:36 +0000

    sloving task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alph = 'a';
+while (alph <= 'z')
+{
+if  (alph != 'e' && alph != 'q')
+putchar(alph);
+alph++;
+
+}
+
+putchar('\n');
+return (0);
+
+}
