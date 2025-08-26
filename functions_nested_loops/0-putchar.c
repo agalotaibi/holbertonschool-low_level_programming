15ebd2d0b4529fee4b8d413fe58cb3a90135a20d
agalotaibi <hopes.gmo@gmail.com>
2025-08-26 13:34:23 +0000

    Sloving task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include 'main.h"
 
 /**
  * main - Entry point
@@ -9,6 +9,14 @@
 
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
