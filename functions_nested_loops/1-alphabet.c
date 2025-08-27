9e32e36a58618992c587cd86895df5a12c2715ab
agalotaibi <hopes.gmo@gmail.com>
2025-08-27 07:01:08 +0000

    sloving task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+char alph = 'a';
+while (alph <= 'z')
+{
+_putchar(alph);
+alph++;
+}
+
+_putchar('\n');
+return (0);
+
+
+}
