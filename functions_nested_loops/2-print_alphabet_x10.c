80937eb1a3876862e81a03646d296d5f4c611ffb
agalotaibi <hopes.gmo@gmail.com>
2025-08-27 07:25:29 +0000

    solve task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,20 +7,17 @@
 
 void print_alphabet_x10(void)
 {
-char alph = 'a';
 int count = 0;
 while (count <= 8)
 {
+char alph = 'a';
 while (alph <= 'z')
 {
 _putchar(alph);
 alph++;
 }
+_putchar('\n');
 count++;
 }
-
-
-
-_putchar('\n');
 
 }
