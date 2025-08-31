bb5db564ede8dad3f8b874aeea8af8e489c62fb3
agalotaibi <hopes.gmo@gmail.com>
2025-08-31 15:26:57 +0000

    solving task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* more_numbers - Prints the numbers 0-14 ten times.
+*/
+
+void print_most_numbers(void)
+{
+int n;
+int count;
+for (count = 1; count <= 10, count++)
+{
+for (n = 0; n <= 14; n++)
+{
+_putchar((n % 10) + '0');
+}
+}
+
+_putchar('\n');
+}
