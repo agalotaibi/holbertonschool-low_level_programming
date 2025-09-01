770062256e177abc081ae99d86b626e0de0c2e59
agalotaibi <hopes.gmo@gmail.com>
2025-09-01 15:46:03 +0000

    solve task 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Taskes a pointer to an int and update
+* the value it pointer to 98
+* @n: the pointer to an int
+*/
+void reset_to_98(int *n)
+{
+
+*n = 98;
+
+}
