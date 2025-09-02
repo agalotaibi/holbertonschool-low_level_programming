287d7a363e039b4b9ee6d3ea4a51037dd1c6e3e0
agalotaibi <hopes.gmo@gmail.com>
2025-09-02 14:43:28 +0000

    solve task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_rev - print the string in reverse.
+* @s: The string to printed in reverse.
+*
+*/
+
+void print_rev(char *s)
+{
+while (*s)
+{
+_putchar(*s--);
+}
+_putchar('\n');
+
+}
