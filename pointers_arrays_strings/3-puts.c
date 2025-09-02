c1222b29cb22cf06efffa59d0c780dc5df1ee299
agalotaibi <hopes.gmo@gmail.com>
2025-09-02 14:38:09 +0000

    solve task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts - print the string.
+* @str: The string to printed.
+*
+*/
+
+void _puts(char *str)
+{
+while (*str)
+{
+__putchar(*str++);
+}
+_putchar('\n');
+
+}
