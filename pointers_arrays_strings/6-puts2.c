10bb874d8e7da5335456b657e5d4ca3adb83cbba
agalotaibi <hopes.gmo@gmail.com>
2025-09-02 15:17:12 +0000

    solve task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* puts2 - print every other string.
+* @str: The string to be printed.
+*
+*/
+
+void puts2(char *str)
+{
+int len = 0,
+int index = 0;
+
+while (str[index++])
+len++;
+
+for (index = 0; index < len; index += 2)
+{
+_putchar(str[index]);
+}
+
+_putchar('\n');
+}
