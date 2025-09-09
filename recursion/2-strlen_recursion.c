edc8ec6a2b489542533cb53cfbf04f77dcf2a317
agalotaibi <hopes.gmo@gmail.com>
2025-09-09 14:38:22 +0000

    solve task 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
-* _strlen_recursion - returns the length of a string
-* @s: string to be counted
-* return the number of string length
-*/
+ * _strlen_recursion - returns the length of a string
+ * @s: the string whose length is to be calculated
+ *
+ * Return: the length of the string
+ */
+
+
 int _strlen_recursion(char *s)
 {
 
