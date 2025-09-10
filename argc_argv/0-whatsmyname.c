b43ae461554482f46b2c4cac56ce61fce36f5bb5
agalotaibi <hopes.gmo@gmail.com>
2025-09-10 14:54:25 +0000

    solve task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,15 +2,15 @@
 #include "main.h"
 
 /**
- * main - Prints the program name, followed by a new line.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- * Return: Always o.
- */
+* main - Prints the program name, followed by a new line.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+* Return: Always o.
+*/
 
 int main(int argc, char *argv[])
 {
-(void)argc; 
+(void)argc;
 printf("%s\n", argv[0]);
 return (0);
 }
