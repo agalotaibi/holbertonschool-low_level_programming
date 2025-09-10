a2a1ed3b19f89fa5fc8d8b0f4dd7aa037e30c07e
agalotaibi <hopes.gmo@gmail.com>
2025-09-10 15:03:52 +0000

    solve task 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Prints the number of arguments passed to it.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+
 int main(int argc, char *argv[])
 {
 (void)argv;
-printf("%d\n", argc);
+printf("%d\n", argc - 1);
 return (0);
 }
