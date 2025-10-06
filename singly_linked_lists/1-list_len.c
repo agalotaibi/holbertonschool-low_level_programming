e52115e4e2c39f42ec68b19cbd76c5f12e922114
agalotaibi <hopes.gmo@gmail.com>
2025-10-06 14:16:16 +0000

    create function count the number of elements in a linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* list_len - Returns the number of elements in a linked list.
+* @h: A pointer to the head of the list_t list.
+*
+* Return: The number of nodes in the list.
+*/
+
+
+size_t list_len(const list_t *h)
+{
+
+size_t count = 0;
+
+const list_t *current = h;
+
+while (current != NULL)
+{
+count++;
+
+current = current->next;
+}
+
+return (count);
+}
