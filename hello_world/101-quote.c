fa9c2fb8c74abeb7e8f936103bf2db3c9786976f
agalotaibi <hopes.gmo@gmail.com>
2025-11-20 15:10:23 +0000

    printing using system call write

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
 
 /**
-* the main is printing the text and that piece of art is useful" - Dora Korpar, 2015-10-19
-* using the system call to write data directly to a file descriptor.
-* Return: always 1
-*/
-
+ * main - Entry point
+ *
+ * Description: Prints a quote to standard error using the write function
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
-    write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
-    return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
